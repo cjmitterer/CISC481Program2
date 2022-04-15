295fb5b633d2a5bdb71f5bbe4da35b7520b01e61
cjmitterer <cjmitter@udel.edu>
2022-04-15 13:51:39 -0400

    Added the constraints file

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+puzzleOne = [[7,    None, None, 4,    None, None, None, 8,    6],
+             [None, 5,    1,    None, 8,    None, 4,    None, None],
+             [None, 4,    None, 3,    None, 7,    None, 9,    None],
+             [3,    None, 9,    None, None, 6,    1,    None, None],
+             [None, None, None, None, 2,    None, None, None, None],
+             [None, None, 4,    9,    None, None, 7,    None, 8],
+             [None, 8,    None, 1,    None, 2,    None, 6,    None],
+             [None, None, 6,    None, 5,    None, 9,    1,    None],
+             [2,    1,    None, None, None, 3,    None, None, 5]]
+puzzleTwo = None
+puzzleThree = None
+puzzleFour = None
+puzzleFive = None
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# 1 TODO
+fourxFourConstraints = {
+
+}
+
+# 1 TODO
+nineXNineConstraints = {
+
+}
+
+# 2 TODO
+def revise(CSP):
+    return None
+
+# 3 TODO
+def AC3(CSP):
+    return None
+
+# 4 TODO
+def minimumRemainingValues(CSP):
+    return None
+
+# 5 TODO
+def backTrackingSearch(CSP):
+    return None
 
 
 # Press the green button in the gutter to run the script.
